09c8d1c953735d1bda5e4e4915cb6bfd7afda5be
stribro <stribrny@1webit.cz>
2014-11-27 12:58:23 +0100

    Added ability to run commands as a specific sudo user

--- config.sample.php
+++ config.sample.php
@@ -3,3 +3,6 @@ define('PATH_GIT_REPO', '/home/projects/eclimbkonfigo.konfigo');
 define('PATH_GITSWITCHER', dirname(__FILE__));
 define('BRANCH_DEFAULT', 'TEST');
 define('GITSWITCHER_PASSWORD', ''); //md5hash
+
+//user to run git commands as. If left empty, run sudo as root
+define('SUDO_USER', '');
