d548db3e90beff47d9757bd3c1a3e378b8b44afd
stribro <stribrny@1webit.cz>
2014-11-27 13:05:49 +0100

    Added documentation to config

--- config.sample.php
+++ config.sample.php
@@ -4,5 +4,16 @@ define('PATH_GITSWITCHER', dirname(__FILE__));
 define('BRANCH_DEFAULT', 'TEST');
 define('GITSWITCHER_PASSWORD', ''); //md5hash
 
-//user to run git commands as. If left empty, run sudo as root
+/*
+ *	user to run git commands as. If left empty, run sudo as root.
+ *
+ * In order for this to work, the sudoers file needs to be edited correctly.
+ *
+ * EXAMPLE(To run git commands as user assembla):
+ * 	// run command: 
+ * 		$ visudo
+ *  // edit sudoers file to include (allow user www-data to sudo git as user assembla without password): 
+ *  	www-data ALL=(assembla) NOPASSWD: /usr/bin/git
+ * 
+ */
 define('SUDO_USER', '');
